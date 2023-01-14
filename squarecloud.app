61021ad66b3a7c6c0b34fb51085d54fd1c1c002f
Gabriel Oliveira <40695127+Polabiel@users.noreply.github.com>
2023-01-13 21:07:42 -0300

    Config restauresds

--- squarecloud.app
+++ squarecloud.app
@@ -1,6 +1,6 @@
 DISPLAY_NAME=Zanoni-BOT | Whatsapp
 DESCRIPTION=Apenas um bot de figurinhas comum de Whatsapp
-MAIN=src/index.js
-MEMORY=100
+MAIN=src\index.js
+MEMORY=500
 AVATAR=https://telegra.ph/file/985dc21e18bd1862114f5.png
 VERSION=recommended
--- src/config.js
+++ src/config.js
@@ -4,12 +4,10 @@ const PREFIX = "/";
 const BOT_EMOJI = "🤖";
 const TEMP_FOLDER = path.resolve(__dirname, "..", "assets", "temp");
 const BOT_NAME = "Zanoni — BOT"
-const OWNER = "https://www.wa.me/+5519981022857"
 
 module.exports = {
   BOT_EMOJI,
   PREFIX,
   TEMP_FOLDER,
   BOT_NAME,
-  OWNER,
 };
